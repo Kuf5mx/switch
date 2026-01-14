c1165ceaad5f6951148b9b7ce4706a3b0887daf9
Code Hawks <rerickg19@gmail.com>
2026-01-14 08:14:35 -0600

    switch
    tutorial para empezar una actividad, lo que almenos deberia tener

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+application {
+    mainClass = "org.example.App"
+}
